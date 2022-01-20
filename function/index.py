eca90141b8838d0aea7990eec5a05f8326555555
aliciousness <84104933+aliciousness@users.noreply.github.com>
2022-01-20 13:51:33 -0500

    took the bot respondes out of the lambda because i was getting an error with the slack module in the fucntion on cloudwatch

--- function/index.py
+++ function/index.py
@@ -1,17 +1,13 @@
-import os,re,slack,uuid,json
+import os,re,uuid,json
+
  
 
 
 CREATE_RAW_PATH = "/challenge"
-SLACK_ACCESS_TOKEN = 'xoxb-2895391715429-2911092400561-olwSyFBzi77lNdFYcMimVMAy'
-APP_VERIF_TOKEN = ''
+
+
 def handler(event, context):
     print(event)
-    
-    #bot responding to channel
-    client = slack.WebClient(token=SLACK_ACCESS_TOKEN)
-    client.chat_postMessage(channel='report-dates', text='This is only a test.')
-
     
     #takes info from body that slack post and returns the challenge key 
     if event['rawPath'] == CREATE_RAW_PATH:
---
+++ function/respond.py
@@ -0,0 +1,9 @@
+#repsonding function
+import slack 
+
+SLACK_ACCESS_TOKEN = 'xoxb-2895391715429-2911092400561-olwSyFBzi77lNdFYcMimVMAy'
+APP_VERIF_TOKEN = ''
+
+client = slack.WebClient(token=SLACK_ACCESS_TOKEN)
+
+client.chat_postMessage(channel='report-dates', text='This is only a test.')
