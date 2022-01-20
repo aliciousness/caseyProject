7388d56e93b1d2629467bc22b5ef48a366b00402
aliciousness <84104933+aliciousness@users.noreply.github.com>
2022-01-20 18:45:02 -0500

    df

--- function/index.py
+++ function/index.py
@@ -7,15 +7,18 @@ CREATE_RAW_PATH = "/challenge"
 
 ok = 'htttp 200 OK'
 def handler(event, context):
-    #print(event)
+    print(event)
     
     #takes info from body that slack post and returns the challenge key 
     if event['rawPath'] == CREATE_RAW_PATH:
         
         print("Start Request")
-        body = event['body']
-        # print('event', event, type(event['body']))
-        print('EVENT TYPE', type(event['body']),event)
+        string = event['body']
+        data = json.loads(string)
+        user = data['event']['user']
+        txt = data['event']['text']
+        msg_id = data['event']['client_msg_id']
+        
         return ok
     
     
