37307f1ed369dd56c9de082dc4ce45ea80826cc0
aliciousness <84104933+aliciousness@users.noreply.github.com>
2022-01-24 21:25:02 -0500

    add gsi to the index but im sure i dont need an a gsi but not sure how to add atrributes to my table on pulumi without having then keyed in the index. will figure that out on a later date but i have the table made and ready to go

--- __main__.py
+++ __main__.py
@@ -42,5 +42,41 @@ apigw = aws.apigatewayv2.Api("httpApiGateway",
     route_key="POST /challenge",
     target=lambda_return.invoke_arn)
 
+#DynamoDB table for reports
+dynamodbReports = aws.dynamodb.Table("dynamoDB-casey-reports",
+    attributes=[
+        aws.dynamodb.TableAttributeArgs(
+            name="lastName",
+            type="S",
+        ),
+        aws.dynamodb.TableAttributeArgs(
+            name="firstName",
+            type="S",
+        ),
+        aws.dynamodb.TableAttributeArgs(
+            name="dateCreated",
+            type="N",
+        ),
+        aws.dynamodb.TableAttributeArgs(
+            name="reportFinished",
+            type="N",
+        ),
+        # aws.dynamodb.TableAttributeArgs(
+        #     name="dateReportFinished",
+        #     type="N",
+        # ),
+    ],
+    billing_mode="PAY_PER_REQUEST",
+    hash_key="lastName",
+    range_key="firstName",
+    global_secondary_indexes=[aws.dynamodb.TableGlobalSecondaryIndexArgs(
+        hash_key="reportFinished",
+        range_key="dateCreated",
+        name="reportFinishedIndex",
+        non_key_attributes=["lastName","firstName","dateReportFinished"],
+        projection_type="INCLUDE"
+    )],
+    )
+
 # Export the API endpoint for easy access
 pulumi.export("endpoint", apigw.api_endpoint)
