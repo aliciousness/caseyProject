894d907e96440a6db802274af68da638544b0440
aliciousness <84104933+aliciousness@users.noreply.github.com>
2022-01-26 11:03:31 -0500

    took out the gsi and just had hash and sort key for the dynamo, commented the other attributes and gsi out and sent to pulumi 

--- __main__.py
+++ __main__.py
@@ -53,14 +53,14 @@ dynamodbReports = aws.dynamodb.Table("dynamoDB-casey-reports",
             name="firstName",
             type="S",
         ),
-        aws.dynamodb.TableAttributeArgs(
-            name="dateCreated",
-            type="N",
-        ),
-        aws.dynamodb.TableAttributeArgs(
-            name="reportFinished",
-            type="N",
-        ),
+        # aws.dynamodb.TableAttributeArgs(
+        #     name="dateCreated",
+        #     type="N",
+        # ),
+        # aws.dynamodb.TableAttributeArgs(
+        #     name="reportFinished",
+        #     type="N",
+        # ),
         # aws.dynamodb.TableAttributeArgs(
         #     name="dateReportFinished",
         #     type="N",
@@ -69,13 +69,13 @@ dynamodbReports = aws.dynamodb.Table("dynamoDB-casey-reports",
     billing_mode="PAY_PER_REQUEST",
     hash_key="lastName",
     range_key="firstName",
-    global_secondary_indexes=[aws.dynamodb.TableGlobalSecondaryIndexArgs(
-        hash_key="reportFinished",
-        range_key="dateCreated",
-        name="reportFinishedIndex",
-        non_key_attributes=["lastName","firstName","dateReportFinished"],
-        projection_type="INCLUDE"
-    )],
+    # global_secondary_indexes=[aws.dynamodb.TableGlobalSecondaryIndexArgs(
+    #     hash_key="reportFinished",
+    #     range_key="dateCreated",
+    #     name="reportFinishedIndex",
+    #     non_key_attributes=["lastName","firstName","dateReportFinished"],
+    #     projection_type="INCLUDE"
+    # )],
     )
 
 # Export the API endpoint for easy access
