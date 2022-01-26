09bde7dd8ebb741fbd25227297c612f045527685
aliciousness <84104933+aliciousness@users.noreply.github.com>
2022-01-26 14:43:52 -0500

    trying to add policy to role having trouble to add new policy to exiting role in pulumi 

--- __main__.py
+++ __main__.py
@@ -17,10 +17,83 @@ lambda_role = aws.iam.Role("lambdaRole",
             }]
     },))
 
+list_read_write_dynamoPolicy = aws.iam.Policy(
+    resource_name = "listReadWriteDynamo",
+    policy =json.dumps( {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:DescribeContributorInsights",
+                "dynamodb:RestoreTableToPointInTime",
+                "dynamodb:UpdateGlobalTable",
+                "dynamodb:UpdateTableReplicaAutoScaling",
+                "dynamodb:DescribeTable",
+                "dynamodb:PartiQLInsert",
+                "dynamodb:GetItem",
+                "dynamodb:DescribeContinuousBackups",
+                "dynamodb:DescribeExport",
+                "dynamodb:EnableKinesisStreamingDestination",
+                "dynamodb:BatchGetItem",
+                "dynamodb:DisableKinesisStreamingDestination",
+                "dynamodb:UpdateTimeToLive",
+                "dynamodb:BatchWriteItem",
+                "dynamodb:PutItem",
+                "dynamodb:PartiQLUpdate",
+                "dynamodb:Scan",
+                "dynamodb:StartAwsBackupJob",
+                "dynamodb:UpdateItem",
+                "dynamodb:UpdateGlobalTableSettings",
+                "dynamodb:CreateTable",
+                "dynamodb:RestoreTableFromAwsBackup",
+                "dynamodb:GetShardIterator",
+                "dynamodb:DescribeReservedCapacity",
+                "dynamodb:ExportTableToPointInTime",
+                "dynamodb:DescribeBackup",
+                "dynamodb:UpdateTable",
+                "dynamodb:GetRecords",
+                "dynamodb:DescribeTableReplicaAutoScaling",
+                "dynamodb:ListTables",
+                "dynamodb:PurchaseReservedCapacityOfferings",
+                "dynamodb:CreateTableReplica",
+                "dynamodb:ListTagsOfResource",
+                "dynamodb:UpdateContributorInsights",
+                "dynamodb:CreateBackup",
+                "dynamodb:UpdateContinuousBackups",
+                "dynamodb:DescribeReservedCapacityOfferings",
+                "dynamodb:PartiQLSelect",
+                "dynamodb:CreateGlobalTable",
+                "dynamodb:DescribeKinesisStreamingDestination",
+                "dynamodb:DescribeLimits",
+                "dynamodb:ListExports",
+                "dynamodb:ConditionCheckItem",
+                "dynamodb:ListBackups",
+                "dynamodb:Query",
+                "dynamodb:DescribeStream",
+                "dynamodb:DescribeTimeToLive",
+                "dynamodb:ListStreams",
+                "dynamodb:ListContributorInsights",
+                "dynamodb:DescribeGlobalTableSettings",
+                "dynamodb:ListGlobalTables",
+                "dynamodb:DescribeGlobalTable",
+                "dynamodb:RestoreTableFromBackup",
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+))
 # Attach policy to the Lambda role created above
 role_policy_attachment = aws.iam.RolePolicyAttachment("lambdaRoleAttachment",
-    role=lambda_role,
+    role=lambda_role, 
     policy_arn=aws.iam.ManagedPolicy.AWS_LAMBDA_BASIC_EXECUTION_ROLE)
+
+role_policy_attachment_table = aws.iam.RolePolicyAttachment(
+    "lambdaRoleAttachment--Table",
+    role=lambda_role.arn,
+    policy_arn=list_read_write_dynamoPolicy.arn)
 
 # Create the lambda to execute
 lambda_return = aws.lambda_.Function("lambdaFunctionReturn", 
